1c8baaa50c9f5dc12f617afbf88a1b3c295c0028
George Tarnaras <georgetarnaras@192.168.1.100>
2023-07-06 18:15:53 +0100

    Updates

--- dummy-dora.py
+++ dummy-dora.py
@@ -72,4 +72,4 @@ if __name__ == '__main__':
     print("First Commit Date:", dora_metrics['first_commit_date'])
     print("Last Commit Date:", dora_metrics['last_commit_date'])
     print("Average Commits per Day:", dora_metrics['average_commits_per_day'])
-    print("Time Between Releases
+    print("Time Between Releases:", dora_metrics['time_between_releases'])
